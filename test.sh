fc1e2c980f746b19d1cd73405afaa3870fc5bf59
tom-ok1 <tom.ok1@icloud.com>
2025-05-04 17:41:43 +0900

    fix: update API proxy target to use production URL and comment out test cases

--- test.sh
+++ test.sh
@@ -7,7 +7,7 @@ curl -G "${API_BASE}/api/combinations" \
   --data-urlencode "duration=5" \
   -s | jq .
 
-echo "Test: HH:MM:SS指定"
-curl -G "${API_BASE}/api/combinations" \
-  --data-urlencode "duration=00:10:30" \
-  -s | jq .
+# echo "Test: HH:MM:SS指定"
+# curl -G "${API_BASE}/api/combinations" \
+#   --data-urlencode "duration=00:10:30" \
+#   -s | jq .
